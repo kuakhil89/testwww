d76b82302b3282525dc6bed0a01cb3056e00ab37
Akhilesh Kumar <akhilesh.kumar@zensar.com>
2018-09-18 17:02:40 +0530

    First commit Message

---
+++ src/main/java/com/cucumber/CucumberTestApplication.java
@@ -0,0 +1,12 @@
+package com.cucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumberTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumberTestApplication.class, args);
+	}
+}
